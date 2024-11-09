e2c6accaa4c88cde73ff0dd1143f64125f798d39
Galagadiv <sobolia37@gmail.com>
2024-11-09 11:56:12 +0200

    begin mobile styling

--- js/clients-slider.js
+++ js/clients-slider.js
@@ -1,8 +1,24 @@
-var clientSlider = new Splide(".splide", {
+var clientSlider = new Splide("#clientsSliderSection", {
 	type: "slide",
-	perPage: 2,
-	focus: "left",
+	perPage: 3,
+	focus: "right",
 	pagination: false,
+	breakpoints: {
+		480: {
+			//
+		},
+	},
 });
 
 clientSlider.mount();
+
+const sourceElement = document.querySelector(".container");
+
+// Елемент, якому передаємо відступ
+const projCont = document.getElementById("project");
+const clientsCont = document.getElementById("client");
+
+// Значення відступу на інший елемент
+const computedStyle = window.getComputedStyle(sourceElement);
+projCont.style.marginLeft = computedStyle.marginLeft;
+clientsCont.style.marginLeft = computedStyle.marginLeft;
