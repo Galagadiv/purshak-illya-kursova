4bd9be4fe43d4869a8295d9c2dfe0d4ee2fefab9
Galagadiv <sobolia37@gmail.com>
2024-11-12 02:19:08 +0200

    80% done mobile

--- js/clients-slider.js
+++ js/clients-slider.js
@@ -1,17 +1,3 @@
-var clientSlider = new Splide("#clientsSliderSection", {
-	type: "slide",
-	perPage: 3,
-	focus: "right",
-	pagination: false,
-	breakpoints: {
-		480: {
-			//
-		},
-	},
-});
-
-clientSlider.mount();
-
 const sourceElement = document.querySelector(".container");
 
 // Елемент, якому передаємо відступ
@@ -20,5 +6,27 @@ const clientsCont = document.getElementById("client");
 
 // Значення відступу на інший елемент
 const computedStyle = window.getComputedStyle(sourceElement);
-projCont.style.marginLeft = computedStyle.marginLeft;
-clientsCont.style.marginLeft = computedStyle.marginLeft;
+// projCont.style.marginLeft = computedStyle.marginLeft;
+// clientsCont.style.marginLeft = computedStyle.marginLeft;
+
+if (window.innerWidth >= 1440) {
+	// Отримуємо значення відступу
+	const computedStyle = window.getComputedStyle(sourceElement);
+	projCont.style.marginLeft = computedStyle.marginLeft;
+	clientsCont.style.marginLeft = computedStyle.marginLeft;
+}
+
+var clientSlider = new Splide("#clientsSliderSection", {
+	type: "slide",
+	perPage: 3,
+	focus: "left",
+	pagination: false,
+	breakpoints: {
+		428: {
+			perPage: 1,
+			arrows: false,
+		},
+	},
+});
+
+clientSlider.mount();
