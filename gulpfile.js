3d477ad079f8356e8addb19cb87d3d5ea3c801a2
Galagadiv <sobolia37@gmail.com>
2024-11-24 22:07:45 +0200

    #! stage building gulp 1:00:00

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,10 @@ const sass = require("gulp-sass")(require("sass"));
 const server = require("gulp-server-livereload");
 const clean = require("gulp-clean");
 const fileSys = require("fs");
+const sourceMaps = require("gulp-sourcemaps");
+const plumber = require("gulp-plumber");
+const notify = require("gulp-notify");
+const error = require("console");
 
 const fileIncludeSettings = {
 	prefix: "@@",
@@ -13,31 +17,56 @@ const severSettings = {
 	livereload: true,
 	open: true,
 };
+const plumberSassConf = {
+	errorHandler: notify.onError({
+		title: "Styles",
+		messege: "Error <%= error.message %>",
+		sound: false,
+	}),
+};
 
-gulp.task("includeFiles", function () {
+gulp.task("html", function () {
 	return gulp
-		.src("./src/*.html")
+		.src("./src/**/*.html")
 		.pipe(fileInclude(fileIncludeSettings))
 		.pipe(gulp.dest("./dist"));
 });
 
 gulp.task("sass", function () {
 	return gulp
-		.src("./src/scss/*.scss")
+		.src("./src/scss/**/*.scss")
+		.pipe(plumber(plumberSassConf))
+		.pipe(sourceMaps.init())
 		.pipe(sass())
+		.pipe(sourceMaps.write())
 		.pipe(gulp.dest("./dist/css/"));
 });
 
-gulp.task("copyImgs", function () {
-	return gulp.src("./images/**/*").pipe(gulp.dest("./dist/img/")); //будь який файл будь яка папка
+gulp.task("images", function () {
+	return gulp.src("./images/**/*").pipe(gulp.dest("./dist/images/")); //будь який файл i будь яка папка
 });
 
-gulp.task("startServer", function () {
-	return gulp.src("./dist/").pipe(server(severSettings));
+gulp.task("server", function () {
+	return gulp.src("./dist").pipe(server(severSettings));
 });
 
-gulp.task("cleanDist", function (done) {
+gulp.task("clean", function (done) {
 	return fileSys.existsSync("./dist/")
-		? gulp.src("./dist/").pipe(clean())
+		? gulp.src("./dist/", {read: false}).pipe(clean({force: true}))
 		: done();
 });
+
+gulp.task("watch", function () {
+	gulp.watch("./src/scss/**/*.scss", gulp.parallel("sass"));
+	gulp.watch("./src/**/*.html", gulp.parallel("html"));
+	gulp.watch("./images/**/*", gulp.parallel("images"));
+});
+
+gulp.task(
+	"default",
+	gulp.series(
+		"clean",
+		gulp.parallel("html", "sass", "images"),
+		gulp.parallel("server", "watch")
+	)
+);
